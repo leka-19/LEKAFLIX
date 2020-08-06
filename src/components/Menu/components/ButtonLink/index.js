0baf8b13eb15cba71ab4679f990225674a2bf8bf
leka-19 <lekacristhine2012@gmail.com>
2020-08-06 18:40:19 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ButtonLink (props){
+    //props=> {class:"o que alguem passar", href: "/"}
+
+
+    return (
+    <a href={props.href} className={props.className}>
+    
+    {props.children}
+ 
+    </a>
+    
+     );
+    
+    }
+    
+    export default ButtonLink;
